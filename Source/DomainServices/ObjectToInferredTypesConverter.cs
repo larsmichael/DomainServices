d71c583c7e6f47246c845209f572f44a36f55672
Lars Michael <lars.michael.dk@gmail.com>
2022-08-05 18:04:53 +0200

    Write modifications

--- Source/DomainServices/ObjectToInferredTypesConverter.cs
+++ Source/DomainServices/ObjectToInferredTypesConverter.cs
@@ -15,24 +15,31 @@ namespace DomainServices
     /// </remarks>
     internal class ObjectToInferredTypesConverter : JsonConverter<object>
     {
-        public override object Read(
-            ref Utf8JsonReader reader,
-            Type typeToConvert,
-            JsonSerializerOptions options) => reader.TokenType switch
+        public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            JsonTokenType.True => true,
-            JsonTokenType.False => false,
-            JsonTokenType.Number when reader.TryGetInt64(out long l) => l,
-            JsonTokenType.Number => reader.GetDouble(),
-            JsonTokenType.String when reader.TryGetDateTime(out DateTime datetime) => datetime,
-            JsonTokenType.String => reader.GetString()!,
-            _ => JsonDocument.ParseValue(ref reader).RootElement.Clone()
-        };
+            return reader.TokenType switch
+            {
+                JsonTokenType.True => true,
+                JsonTokenType.False => false,
+                JsonTokenType.Number when reader.TryGetInt64(out long l) => l,
+                JsonTokenType.Number => reader.GetDouble(),
+                JsonTokenType.String when reader.TryGetDateTime(out DateTime datetime) => datetime,
+                JsonTokenType.String => reader.GetString()!,
+                _ => JsonDocument.ParseValue(ref reader).RootElement.Clone()
+            };
+        }
 
-        public override void Write(
-            Utf8JsonWriter writer,
-            object objectToWrite,
-            JsonSerializerOptions options) =>
-            JsonSerializer.Serialize(writer, objectToWrite, objectToWrite.GetType(), options);
+        public override void Write(Utf8JsonWriter writer, object objectToWrite, JsonSerializerOptions options)
+        {
+            if (objectToWrite.GetType() == typeof(object))
+            {
+                writer.WriteStartObject();
+                writer.WriteEndObject();
+            }
+            else
+            {
+                JsonSerializer.Serialize(writer, objectToWrite, objectToWrite.GetType(), options);
+            }
+        }
     }
 }
